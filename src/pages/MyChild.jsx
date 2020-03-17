d95c8a74e4d4b0dbb82a37df7f818a4d4f86b579
cmejones <1016cme@gmail.com>
2020-03-17 19:27:12 -0400

    fixed componentDidMount on MyChild.jsx

--- src/pages/MyChild.jsx
+++ src/pages/MyChild.jsx
@@ -4,38 +4,12 @@ import { db } from '../firebase/firebase';
 import { connect } from 'react-redux';
 
 
-
 function mapStateToProps(state) { //need to render redux store
     return {
         user: state.auth.user.uid
        // userName: state.auth.user.firstName + state.auth.user.lastName
     };
 }
-
-// function getChildren() {
-//     const children = [];
-//     db.collection('children') //add where clause for parentid
-//         .get()
-//         .then(function(querySnapshot) {
-//             querySnapshot.forEach(function(doc) {
-//                 children.push({
-//                     id:doc.id,
-//                     ...doc.data()
-//             });
-//                 console.log(doc.id, "=>", doc.data());
-//                 // this.setState = {
-//                 //     id: doc.id,
-//                 //     data: doc.data()
-//                 // }
-//                 return children
-//             });            
-//         })
-//         .catch(function(error) {
-//             console.log('error getting documents: ', error);
-//         });
-// }
-
-
 
 class MyChild extends React.Component {
     constructor(props) {
@@ -46,50 +20,47 @@ class MyChild extends React.Component {
         }
     }
 
-
-    componentDidMount() {
-            function getChildren() {
-            console.log('here');
-
-            const childrenRef = db.collection('children');
-            let children = [];
-            let allChildren = childrenRef.get()
-            .then(snapshot => {
-                snapshot.forEach(doc => {
-                    children.push({
-                        id:doc.id,
-                        ...doc.data()
-                    });
-                    console.log(doc.id, '=>', doc.data()); //showing children
-                    console.log(children, 'children')
-                });
-            })
-            .catch(err => {
-                console.log('error getting children information', err);
-            })
-            console.log(children);
-            return allChildren;
-        }
+    async getChildren() {
+        
     }
-//     componentDidMount() {
-//         let allChildren = getChildren();
-        
-//         this.setState({
-//             children: allChildren 
-//             //this is empty; 
-        
-//         });
-//         console.log(this.state);
+    componentDidMount() {
+        console.log('here');
 
-// }
+        const childrenRef = db.collection('children')
+    
+        let allChildren = childrenRef.get()
+        .then(snapshot => {
+            console.log(snapshot);
+            let children = [];
+            snapshot.forEach(doc => {
+                children.push({
+                    id:doc.id,
+                    ...doc.data()
+                });
+                console.log(doc.id, '=>', doc.data()); //showing children
+                console.log(children, 'children')
+            });
+            this.setState({
+                children: children
+            })
+        })
+        .catch(err => {
+            console.log('error getting children information', err);
+        })
+    }
 
         render() {
+            const {  children } = this.state;
+        
+            const myChildren = this.state.children.map((child) => {
+            return <div key={child.id} {...child}></div>
+        });
             console.log(this.state);
             //const allChildren = this.state;
             return (
                 <section className='display-item'>
                     <div className="wrapper">
-                        {/* <div>{allChildren}</div> */}
+                        <div>{myChildren}</div>
                         <div>hello</div>
                     </div>
                 </section>
