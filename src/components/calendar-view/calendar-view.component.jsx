4bc2434493edf3444746b2cc9fdcf3c0e99cc0cd
Leah H <harrichl@gmail.com>
2020-03-20 16:54:13 -0400

    add events to db by user

--- src/components/calendar-view/calendar-view.component.jsx
+++ src/components/calendar-view/calendar-view.component.jsx
@@ -3,14 +3,23 @@ import React from 'react';
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
+import { connect } from "react-redux";
+import { db } from '../../firebase/firebase';
+
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
+function mapStateToProps(state) {
+    return {
+        user: state.auth.user   
+    };
+}
+
 const propTypes = {}
 class MyCalendar extends React.Component {
-    constructor(...args) {
-        super(...args);
+    constructor(...props) {
+        super(...props);
     
         this.state = {
             events: []
@@ -18,18 +27,35 @@ class MyCalendar extends React.Component {
     }
 
     handleSelect = ({ start, end }) => {
-    const title = window.prompt('New Event name')
-    if (title)
-      this.setState({
-        events: [
-          ...this.state.events,
-          {
-            start,
-            end,
-            title,
-          },
-        ],
-      })
+
+         const { user } = this.props
+        console.log('userId handleSelect: ', user.uid)
+
+        let events = []
+        const title = window.prompt('New Event name')
+        if (title)
+        this.setState({
+            events: [
+            ...this.state.events,
+            {
+                start,
+                end,
+                title,
+                
+                },
+        
+            ], 
+          
+        })
+            events.push(this.state.events);
+            console.log('handle select state: ', events) 
+        
+            // convert events array to object for firestore
+            let data = {...events}
+            // Add events to Firestore by user 
+        db.collection("events").doc(user.uid)
+            .collection("myEvents").doc().set(data, { merge: true })   
+        
   }
 
     render() {
@@ -41,7 +67,7 @@ class MyCalendar extends React.Component {
                         selectable
                         localizer={localizer}
                         defaultDate={new Date()}
-                        defaultView={Views.WEEK}
+                        defaultView={Views.MONTH}
                         events={this.state.events}
                         style={{ height: "100vh", width: "80vw" }} 
                         onSelectEvent={event => alert(event.title)}//create function here to add form
@@ -55,4 +81,4 @@ class MyCalendar extends React.Component {
 
 MyCalendar.propTypes = propTypes
 
-export default MyCalendar;
+export default connect(mapStateToProps)(MyCalendar);
