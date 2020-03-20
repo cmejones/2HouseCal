a83e3edfca99a19ec4f40681b56c2660f63bd4c5
cmejones <1016cme@gmail.com>
2020-03-19 20:52:52 -0400

    add modal to calendar-view

--- src/components/calendar-view/calendar-view.component.jsx
+++ src/components/calendar-view/calendar-view.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
-// import events from '../events'
+import { db } from '../../firebase/firebase';
 import ExampleControlSlot from './ExampleControlSlot'
 import moment from 'moment';
+import Modal from 'react-modal';
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import './calendar-view.styles.css';
@@ -16,23 +17,59 @@ class CalendarView extends React.Component {
     constructor(...args) {
         super(...args)
 
-        this.state = { events: [] }
+        this.state = { events: [], description: '' }
     }
 
-    handleSelect = ({ start, end }) => {
-        const title = window.prompt('New Event name')
-        if (title)
-            this.setState({
-                events: [
-                    ...this.state.events,
-                {
-                    start,
-                    end,
-                    title,
-                },
-            ],
-        })
-    }
+    // handleDate = ({ start, end }) => {
+    //     <form>
+    //         <input
+    //         className="input" 
+    //         name="eventTitle" 
+    //         component="input" 
+    //         type="text" 
+    //         value={this.state.title}
+    //         onChange={this.handleChange}
+    //         label="Event Title"/>
+                            
+    //     </form>
+    // }
+handleSelect = ({ start, end }) => {
+  //set model to true
+    this.setState({
+        modalIsOpen: true,
+        start,
+        end
+    });
+}
+
+renderModal = () => {
+    if (!this.state.modalIsOpen) return;
+    return(
+        <Modal
+        isOpen={this.state.modalIsOpen}
+        onAfterOpen={this.afterOpenModal}
+        onRequestClose={this.closeModal}
+        contentLabel="Example Modal"
+        >
+
+            <h2>Add event</h2>
+            <form onSubmit={this.onFormSubmit}>
+                <input   
+                    className="input" 
+                    name="title" 
+                    component="input" 
+                    type="text" 
+                    value={this.state.title}
+                    //onChange={this.handleChange}
+                    label="Title"/>
+
+
+                <input type="submit" value="Submit" /><br />
+                <button onClick={this.closeModal}>close</button>
+            </form>
+        </Modal>
+    );
+}
 
     render() {
         console.log(this.state);
@@ -46,6 +83,8 @@ class CalendarView extends React.Component {
                 </strong>
             </ExampleControlSlot.Entry>
             <Calendar
+                style={{ height: "100vh", width: "80vw" }}
+                //onSelectSlot={(data) => {console.log(data)}} //create function here to add form
                 selectable
                 localizer={localizer}
                 events={this.state.events}
@@ -55,6 +94,8 @@ class CalendarView extends React.Component {
                 onSelectEvent={event => alert(event.title)}
                 onSelectSlot={this.handleSelect}
             />
+                <button onClick={this.openModal}>Open Modal</button>
+                {this.renderModal()}
         </>
         )
     }
