3a847e42fa910b9868756ba1423b9a42a33f3225
cmejones <1016cme@gmail.com>
2020-03-21 10:03:52 -0400

    add header to updateChild

--- src/pages/UpdateChildPage.jsx
+++ src/pages/UpdateChildPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import UpdateChild from '../components/Forms/UpdateChild';
+import Header from '../components/header/header.component';
 
 const UpdateChildForm = () => (
     <div className='flex-container wrap'>
+        <Header />
         <UpdateChild />
     </div>
 );
