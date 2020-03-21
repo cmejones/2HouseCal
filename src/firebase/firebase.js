1c8acf3d3eeed6509077a35e8a8210721e929bf4
cmejones <1016cme@gmail.com>
2020-03-21 09:02:09 -0400

    update env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,6 +4,7 @@ import 'firebase/auth' ;
 import 'firebase/storage';
 require('dotenv').config()
 
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
