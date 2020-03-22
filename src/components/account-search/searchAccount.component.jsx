bf7ec75847b74e9d9b39f6bc83b8f504510cf4cf
cmejones <1016cme@gmail.com>
2020-03-22 12:57:14 -0400

    querySnapshot working

--- src/components/account-search/searchAccount.component.jsx
+++ src/components/account-search/searchAccount.component.jsx
@@ -44,55 +44,59 @@ class AccountSearch extends React.Component {
     }
 
 
-    fetchEmailSearchResults = async event => {
+    fetchEmailSearchResults = (event) => {
         event.preventDefault();
 
         console.log("email: ", this.state.email)
-        const userEmail = this.state.email;
+        const email = this.state.email;
 
-        const usersRef = db.collection("users").where("email", "==", userEmail);
+        const usersRef = db.collection("users").where("email", "==", email);
     
         usersRef.get()
             .then(function(querySnapshot) {
                 querySnapshot.forEach(function(doc) {
-                    console.log(doc.id, '=>', doc.data());
-                    const data = doc.data();
-                    console.log(data.email);
-                    this.setState({
-                        email: data.email,
-                        loading: false
-                    })
+                    //console.log(doc.id, '=>', doc.data());
+                    if(doc.exists) {
+                        const data = doc.data();
+                        console.log(data.email);
+                    } else {
+                        alert('no such email')
+                        console.log('no doc')
+                    }
                 })
+                //.catch(function(error) {
+               // console.log('Error getting user by email', error);
             })
-            .catch(function(error) {
-                console.log('Error getting user by email', error);
-            });
+            this.setState({
+                email: email,
+                loading: false
+            })
+            //})
         }
     
         render() {
-            //const email  = this.state.email;
-
-            console.warn("warn this.state: ", this.state.email)
+            const email  = this.state.email;
+            //console.log(this.state.email);
+            //console.warn("warn this.state: ", this.state.email)
             return (
-            <div className="container">
-                <form className='search-form' noValidate autoComplete="off" onSubmit={this.fetchEmailSearchResults}>
-                
-                    <label className="search-label" htmlFor="search-email">
-                        <input
-                            type="text"
-                            name="email"
-                            value={this.state.email}
-                            id="search-email"
-                            placeholder="Search by email..."
-                            onChange={this.handleChange}
-                            
-                        />
-                        <SearchIcon className="search-icon" fontSize="large"  />   
-                    </label>
-                </form>
-            </div>
-    
-        )
+                <div className="container">
+                    <form className='search-form' noValidate autoComplete="off" onSubmit={this.fetchEmailSearchResults}>
+                    
+                        <label className="search-label" htmlFor="search-email">
+                            <input
+                                type="text"
+                                name="email"
+                                value={this.state.email}
+                                id="search-email"
+                                placeholder="Search by email..."
+                                onChange={this.handleChange}
+                                
+                            />
+                            <SearchIcon className="search-icon" fontSize="large"  />   
+                        </label>
+                    </form>
+                </div>
+            )
         }
     }
 
