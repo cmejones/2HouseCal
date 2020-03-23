92d71401b9deb6dddb3f9aee4a7e445df24db194
Leah H <harrichl@gmail.com>
2020-03-23 03:47:42 -0400

    account email search working

--- src/components/account-search/seachAccount.component.jsx
+++ src/components/account-search/seachAccount.component.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { connect } from "react-redux";
-import { db } from '../../firebase/firebase';
-
+// import { db } from '../../firebase/firebase';
+import axios from 'axios';
 
 import SearchIcon from '@material-ui/icons/Search';
-
+// import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import './searchAccount.styles.css'
+
+
 
 function mapStateToProps(state) {
     return {
@@ -13,75 +20,114 @@ function mapStateToProps(state) {
         
     };
 }
-
 class AccountSearch extends React.Component {
     constructor(props) {
         super(props);
 
         this.state={
-            email:'',
+            query:'',
             results: '',
             loading: false,
             message: ''
         }
     
+        this.cancel = '';
         this.handleChange = this.handleChange.bind(this);
-        this.fetchEmailSearchResults = this.fetchEmailSearchResults.bind(this);
-        
+        this.fetchEmailSearchResults = this.fetchEmailSearchResults.bind(this);    
     }
 
    handleChange = (event) => {
 
-        const email = event.target.value;
-        console.log(email);
+        const query = event.target.value;
+    
         this.setState({
-            email: email,
+            query: query,
             loading: true,
             message: ''
-        });
-        
+        }, () => {
+            this.fetchEmailSearchResults(query)
+        })
+       
     }
 
-    async fetchEmailSearchResults(email){
-   
-        console.log("email: ", email)
+    fetchEmailSearchResults = (query) =>{
+        
+        console.log("email: ", query)
 
-        const usersRef = await db.collection("users").where("email", "==", email);
-    
-        usersRef.get()
-            .then(function(querySnapshot) {
-                if(querySnapshot) {
-                    console.log(querySnapshot);
-                    this.setState({
-                        email: email,
-            
-                })
-                 
-                } else {
-                    console.log('No user found!')
-                }
-                })
-            .catch(err => {
-                console.log('Error getting user by email', err);
-            });
+        let searchUrl = `https://api-2housecal.herokuapp.com/api/users/search/${query}` 
 
-            console.log("this.state after query:", this.state)
-            
+        // cancel previous request; see axios cancel token
+        if(this.cancel !== ''){
+            this.cancel.cancel();
         }
+        
+        // save new request for cancellation
+        this.cancel = axios.CancelToken.source();
+      
+        
+        axios.get(searchUrl, {
+            // cancel toke use by axios
+            cancelToken: this.cancel.token   
+        })
+        .then( response => {
+            const resultNotFoundMsg = ! response.data ? 'No user found' : '';
+            console.log(response.data);
+            this.setState({
+                results: response.data,
+                message: resultNotFoundMsg, 
+                loading: false
+            })
+        })
+        .catch(error => {
+            if( axios.isCancel(error) || error ) {
+                this.setState({
+                    loading: false, 
+                    message: 'Failed to fetch data. Please check network.'
+                })
+            }
+        })  
+      
+    }
+
+    renderEmailSearchResults = () => {
+        const { results } = this.state;
+        console.log(results);
+        if(results) {
+            return (
+                <Card className="root">
+      <CardContent>
+        <Typography className="title" color="textSecondary" gutterBottom>
+          User Found: 
+        </Typography>
+        <Typography variant="h5" component="h2">
+                email: {results.email}
+        </Typography>
+        <Typography className="pos" color="textSecondary">
+          username: {results.displayName}
+        </Typography>
+        
+      </CardContent>
+      <CardActions>
+        <Button size="medium">Add Family</Button>
+      </CardActions>
+    </Card>
+            )
+        }
+    };
        
         render() {
-            const { email } = this.state;
+            const { query } = this.state;
 
             console.warn("warn this.state: ", this.state)
             return (
             <div className="container">
-                <form className='search-form' noValidate autoComplete="off" onSubmit={this.fetchEmailSearchResults}>
+                <form className='search-form' noValidate autoComplete="off" >
                 
                     <label className="search-label" htmlFor="search-email">
                         <input
                             type="text"
-                            name="email"
-                            value={email}
+                            name="query"
+                            value={query}
                             id="search-email"
                             placeholder="Search by email..."
                            onChange={this.handleChange}
@@ -90,6 +136,11 @@ class AccountSearch extends React.Component {
                         <SearchIcon className="search-icon" fontSize="large"  />   
                     </label>
                 </form>
+
+                {/* Results */}
+                <div className="results-container">
+                {this.renderEmailSearchResults()}
+                </div>
             </div>
            
         )
